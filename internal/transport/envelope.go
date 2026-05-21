52fb6efb29abc374a4ba490ab26eff8e69d8f21f
Mralimoh <mralimoh@github.com>
2026-05-21 12:20:35 +0330

    Minor updates

--- internal/transport/envelope.go
+++ internal/transport/envelope.go
@@ -22,114 +22,85 @@ const (
 )
 
 func (e *Envelope) Encode(w io.Writer) error {
-	if len(e.SessionID) > MaxSessionIDLen {
-		return fmt.Errorf("session ID too long: %d", len(e.SessionID))
-	}
-	if len(e.TargetAddr) > MaxAddrLen {
-		return fmt.Errorf("target address too long: %d", len(e.TargetAddr))
-	}
-	if len(e.Payload) > MaxPayloadLen {
-		return fmt.Errorf("payload too large: %d", len(e.Payload))
-	}
-
-	headerSize := 1 + 1 + len(e.SessionID) + 8 + 1 + len(e.TargetAddr) + 1 + 4
-	var hdrBuf [512]byte
-	hdr := hdrBuf[:headerSize]
+	metaSize := 1 + 1 + len(e.SessionID) + 8 + 1 + len(e.TargetAddr) + 1 + 4
 	
-	hdr[0] = MagicByte
-	hdr[1] = uint8(len(e.SessionID))
+	buf := make([]byte, 2 + metaSize)
+	binary.BigEndian.PutUint16(buf[0:2], uint16(metaSize))
+	
 	offset := 2
-	copy(hdr[offset:], e.SessionID)
+	buf[offset] = MagicByte
+	buf[offset+1] = uint8(len(e.SessionID))
+	offset += 2
+	copy(buf[offset:], e.SessionID)
 	offset += len(e.SessionID)
 
-	binary.BigEndian.PutUint64(hdr[offset:], e.Seq)
+	binary.BigEndian.PutUint64(buf[offset:], e.Seq)
 	offset += 8
 
-	hdr[offset] = uint8(len(e.TargetAddr))
+	buf[offset] = uint8(len(e.TargetAddr))
 	offset++
-	copy(hdr[offset:], e.TargetAddr)
+	copy(buf[offset:], e.TargetAddr)
 	offset += len(e.TargetAddr)
 
 	if e.Close {
-		hdr[offset] = 1
+		buf[offset] = 1
 	} else {
-		hdr[offset] = 0
+		buf[offset] = 0
 	}
 	offset++
 
-	binary.BigEndian.PutUint32(hdr[offset:], uint32(len(e.Payload)))
+	binary.BigEndian.PutUint32(buf[offset:], uint32(len(e.Payload)))
 	
-	if _, err := w.Write(hdr); err != nil {
+	if _, err := w.Write(buf); err != nil {
 		return err
 	}
 	if len(e.Payload) > 0 {
 		_, err := w.Write(e.Payload)
-		return err
 	}
 	return nil
 }
 
 func (e *Envelope) Decode(r io.Reader) error {
-	var prefix [2]byte
-	if _, err := io.ReadFull(r, prefix[:]); err != nil {
+	var sizeBuf [2]byte
+	if _, err := io.ReadFull(r, sizeBuf[:]); err != nil {
 		return err
 	}
-	if prefix[0] != MagicByte {
-		return fmt.Errorf("invalid magic byte: 0x%X", prefix[0])
-	}
+	metaSize := int(binary.BigEndian.Uint16(sizeBuf[:]))
 
-	sidLen := int(prefix[1])
-	if sidLen > MaxSessionIDLen {
-		return fmt.Errorf("malicious session ID length: %d", sidLen)
-	}
-
-	sidBuf := make([]byte, sidLen)
-	if _, err := io.ReadFull(r, sidBuf); err != nil {
+	buf := make([]byte, metaSize)
+	if _, err := io.ReadFull(r, buf); err != nil {
 		return err
 	}
-	e.SessionID = string(sidBuf)
 
-	var seqBuf [8]byte
-	if _, err := io.ReadFull(r, seqBuf[:]); err != nil {
-		return err
+	if buf[0] != MagicByte {
+		return fmt.Errorf("invalid magic byte")
 	}
-	e.Seq = binary.BigEndian.Uint64(seqBuf[:])
 
-	var addrLenBuf [1]byte
-	if _, err := io.ReadFull(r, addrLenBuf[:]); err != nil {
-		return err
-	}
-	addrLen := int(addrLenBuf[0])
-	if addrLen > MaxAddrLen {
-		return fmt.Errorf("malicious address length: %d", addrLen)
-	}
+	sidLen := int(buf[1])
+	e.SessionID = string(buf[2 : 2+sidLen])
 	
+	offset := 2 + sidLen
+	e.Seq = binary.BigEndian.Uint64(buf[offset : offset+8])
+	offset += 8
+	
+	addrLen := int(buf[offset])
+	offset++
 	if addrLen > 0 {
-		addrBuf := make([]byte, addrLen)
-		if _, err := io.ReadFull(r, addrBuf); err != nil {
-			return err
-		}
-		e.TargetAddr = string(addrBuf)
+		e.TargetAddr = string(buf[offset : offset+addrLen])
+		offset += addrLen
 	}
 
-	var footer [5]byte
-	if _, err := io.ReadFull(r, footer[:]); err != nil {
-		return err
-	}
-	e.Close = footer[0] == 1
-	payLen := binary.BigEndian.Uint32(footer[1:])
-
-	if payLen > MaxPayloadLen {
-		return fmt.Errorf("packet too large: %d", payLen)
-	}
+	e.Close = buf[offset] == 1
+	payLen := binary.BigEndian.Uint32(buf[offset+1:])
 
 	if payLen > 0 {
+		if payLen > MaxPayloadLen {
+			return fmt.Errorf("packet too large: %d", payLen)
+		}
 		e.Payload = make([]byte, payLen)
 		if _, err := io.ReadFull(r, e.Payload); err != nil {
 			return err
 		}
-	} else {
-		e.Payload = nil
 	}
 	return nil
 }
